d32b514242ec397c1dae771d372e1a8afa08a083
Brendon Lee <144931412+bRENDON-lEE10@users.noreply.github.com>
2024-03-28 17:06:59 -0600

    Update main.js

--- main.js
+++ main.js
@@ -41,6 +41,20 @@ console.log(myIndexOf(["a","b","c"],"b"));//1
 console.log(myIndexOf(["a","b","c"],"c"));//2
 console.log(myIndexOf(["a","b","c"],"d"));//-1
 
+// Average Functions
+function average(numbers){
+  let sum = 0
+
+  for(let i = 0; i <numbers.length; i++){
+    sum += numbers[i];
+  }
+  return sum/ numbers.length;
+}
+console.log(average([1,2,3,4,5]));//3
+console.log(average([7,7,7,7,7,7,7]));//7
+console.log(average([-10,-5,0,5,10]));//0
+console.log(average([90,84,65,73,94]));//81.2
+
 // Count Function
 function count(array, item){
   let count = 0;
@@ -71,3 +85,21 @@ console.log(Math.max(-50,-40,-30,-20));
 console.log(Math.max(-20,-30,-40,-50));
 // Expected output: -20
 
+// Swap Function
+function swap(array, index1, index2){
+  let copy = [];
+
+  for (let i = 0; i < array.length; i ++){
+    if (i === index1){
+      copy.push( array[index2]);
+    }else if (i === index2){
+      copy.push(array[index1]);
+    }else{
+      copy.push(array[i]);
+    }
+  }
+  return copy;
+}
+
+console.log(swap([2,4,6,8,10],2,4));//[2,4,10,8,6]
+console.log(swap(["a","b", "c", "d","e"],0,1));//[`b`,`a`,`c`,`d`,`e`]
